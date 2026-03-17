35d61f9192cdd4b41a104106596c71ccde40856e
M.S. Shruthi <msshruthi432@gmail.com>
2026-03-17 16:53:19 +0530

    added navigatoin bar

--- script.js
+++ script.js
@@ -137,3 +137,9 @@ window.onclick = function(event) {
     linkedinPopup.style.display = "none";
   }
 }
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
